00a64e8cb5d4f18799c6471e3c4793fe9154a169
JaXYaXJJ <ja-ya@mail.ru>
2023-09-04 23:37:40 +0300

    Comments and users relationship

--- src/main/java/edu/hackeru/evgenyzakalinsky/restour/repository/CommentRepository.java
+++ src/main/java/edu/hackeru/evgenyzakalinsky/restour/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package edu.hackeru.evgenyzakalinsky.restour.repository;
 
 import edu.hackeru.evgenyzakalinsky.restour.entity.Comment;
-import edu.hackeru.evgenyzakalinsky.restour.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
